f63653adaab7525c4baeb0a856de45191b5c42da
The Bearodactyl <dabewodactowluwu@gmail.com>
2025-12-12 20:29:54 -0600

    add minmax macro

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,9 @@ mod builder_lite;
 #[cfg(feature = "doc_display")]
 mod doc_display;
 
+#[cfg(feature = "minmax")]
+mod minmax;
+
 #[cfg(feature = "builder_lite")]
 #[proc_macro_derive(BuilderLite, attributes(builder))]
 /// Automatically implements the builder lite pattern for a struct
@@ -24,6 +27,20 @@ pub fn derive_doc_display(input: proc_macro::TokenStream) -> proc_macro::TokenSt
     let input = syn::parse_macro_input!(input as syn::DeriveInput);
 
     doc_display::expand_doc_display(input)
+        .unwrap_or_else(|err| err.to_compile_error())
+        .into()
+}
+
+#[cfg(feature = "minmax")]
+#[proc_macro_attribute]
+/// Validates integer function parameters against minimum and maximum bounds at compile-time.
+pub fn minmax(
+    _attr: proc_macro::TokenStream,
+    item: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    let input = syn::parse_macro_input!(item as syn::ItemFn);
+
+    minmax::expand_minmax(input)
         .unwrap_or_else(|err| err.to_compile_error())
         .into()
 }
